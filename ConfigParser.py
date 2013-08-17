3647a4501a0e1370958cca3734cedae2ef916487
Song Qiang <qiang.song@usc.edu>
2013-08-16 18:30:43 -0700

    add more methods similar to the standard one

--- ConfigParser.py
+++ ConfigParser.py
@@ -13,29 +13,66 @@ class ConfigParser:
         """
         read configuration file
         """
-        self.items = {}
+        self.noSectionItems = {}
+        self.sectionIterms = {}
+
+        section = ""
+        
         for line in open(fn):
             line = line.strip()
             comment_start = line.find("#")
             if comment_start != -1:
                 line = line[:comment_start]
 
-            fields = []
-            if line.find("=") != -1:
-                fields = line.split("=", 1)
-            elif line.find(":") != -1:
-                fields = line.split(":", 1)
-            elif line.find("\t") != -1:
-                fields = line.split("\t", 1)
-            elif line.find(" ") != -1:
-                fields = line.split(" ", 1)
-
-                
-            if len(fields) == 2:
-                fields = [i.strip() for i in fields]
-                if fields[0] and fields[1]:
-                    self.items.update([(fields[0], fields[1])])
+            leftsquare = line.find("[")
+            rightsquare = line.find("]")
+            if leftsquare != -1 and rightsquare != -1 and rightsquare > leftsquare:
+                line = line[leftsquare:(rightsquare + 1)].strip()
+                if line and not line in sectionIterms:
+                    section = line
+                    self.sectionIterms.update([(section, {})])
+            else:
+                fields = []
+                if line.find("=") != -1:
+                    fields = line.split("=", 1)
+                elif line.find(":") != -1:
+                    fields = line.split(":", 1)
+                elif line.find("\t") != -1:
+                    fields = line.split("\t", 1)
+                elif line.find(" ") != -1:
+                    fields = line.split(" ", 1)
+                    
+                if len(fields) == 2:
+                    fields = [i.strip() for i in fields]
+                    if fields[0] and fields[1]:
+                        if section:
+                            self.sectionIterms[section].update(\
+                                        [(fields[0], fields[1])])
+                        else:
+                            self.noSectionItems.update([(fields[0], fields[1])])
     
     def __init__(self, fn):
         self.read(fn)
 
+    def items(self, section = ""):
+        if section and section in self.sectionIterms:
+            return [(k, v) for k, v in self.sectionIterms[section]]
+        else:
+            return [(k, v) for k, v in self.noSectionItems]
+
+    def options(self, section = ""):
+        if section and section in self.sectionIterms:
+            return self.sectionIterms[section].keys()
+        else:
+            return self.noSectionItems.keys()
+
+    def sections(self):
+        return [""] + self.sectionIterms.keys()
+
+
+    def get(self, section = "", option = ""):
+        if section and section in self.sectionIterms:
+            return self.sectionIterms[section][option]
+        else:
+            return self.noSectionIterms[option]
+            
